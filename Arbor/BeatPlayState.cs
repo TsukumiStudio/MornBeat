7457b0ff9bd4c6f79faaa1415d9afef2ab491d3e
matsufriends <pikapikan0526@gmail.com>
2026-03-07 16:50:20 +0900

    Arborの移植

---
+++ Arbor/BeatPlayState.cs
@@ -0,0 +1,23 @@
+﻿#if USE_ARBOR
+using Arbor;
+using UnityEngine;
+using VContainer;
+
+namespace MornLib
+{
+    internal class BeatPlayState : StateBehaviour
+    {
+        [SerializeField] private MornBeatMemoSo _beatMemo;
+        [SerializeField] private bool _executeIsolated;
+        [SerializeField] private StateLink _onComplete;
+        [Inject] private MornBeatControllerMono _beatController;
+
+        public override async void OnStateBegin()
+        {
+            var ct = _executeIsolated ? MornApp.QuitToken : CancellationTokenOnEnd;
+            await _beatController.StartAsync(new MornBeatStartInfo { BeatMemo = _beatMemo, Ct = ct, });
+            Transition(_onComplete);
+        }
+    }
+}
+#endif
