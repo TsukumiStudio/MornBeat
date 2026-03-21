375ad475cdad4ef476db3881b45729f72be8a3f0
matsufriends <pikapikan0526@gmail.com>
2026-03-22 00:29:16 +0900

    MornBeatScaler: RectTransformのアスペクト比に応じて拡大係数を補正

--- VContainer/MornBeatScaler.cs
+++ VContainer/MornBeatScaler.cs
@@ -10,18 +10,41 @@ namespace MornLib
         [SerializeField, HelpBox("未設定時はMornBeatGlobalのデフォルトを使用")]
         private MornBeatScaleSettings _settings;
         private Vector3 _originScale;
+        private Vector3 _adjustedAimScale;
 
         private void Start()
         {
             if (_settings == null) _settings = MornBeatGlobal.I.DefaultScaleSettings;
             _originScale = transform.localScale;
-            _beatController.PlayModule.OnBeat.Where(x => x.IsJustForAnyBeat(_settings.PerBeat)).Subscribe(_ => transform.localScale = _settings.AimScale).AddTo(this);
+            _adjustedAimScale = CalcAdjustedAimScale();
+            _beatController.PlayModule.OnBeat
+                .Where(x => x.IsJustForAnyBeat(_settings.PerBeat))
+                .Subscribe(_ => transform.localScale = _adjustedAimScale)
+                .AddTo(this);
         }
 
         private void Update()
         {
             var scale = Vector3.Lerp(transform.localScale, _originScale, Time.deltaTime * _settings.LerpSpeed);
             transform.localScale = scale;
+        }
+
+        private Vector3 CalcAdjustedAimScale()
+        {
+            var aim = _settings.AimScale;
+            var rt = transform as RectTransform;
+            if (rt == null) return aim;
+
+            var size = rt.sizeDelta;
+            if (size.x <= 0 || size.y <= 0) return aim;
+
+            var minSide = Mathf.Min(size.x, size.y);
+            var ratioX = minSide / size.x;
+            var ratioY = minSide / size.y;
+            return new Vector3(
+                1f + (aim.x - 1f) * ratioX,
+                1f + (aim.y - 1f) * ratioY,
+                aim.z);
         }
     }
 }
