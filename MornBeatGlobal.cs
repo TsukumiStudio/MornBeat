0dc50e2c0a960e5d030dab8897183939f8d76157
matsufriends <pikapikan0526@gmail.com>
2026-04-11 21:15:27 +0900

    MornGlobal API変更に追従 (using/Logger/SetDirty)

--- MornBeatGlobal.cs
+++ MornBeatGlobal.cs
@@ -1,4 +1,4 @@
-using MornGlobal;
+using MornLib;
 using UnityEngine;
 
 namespace MornBeat
@@ -9,22 +9,22 @@ namespace MornBeat
 
         internal static void Log(string message)
         {
-            I.LogInternal(message);
+            Logger.Log(message);
         }
 
         internal static void LogWarning(string message)
         {
-            I.LogWarningInternal(message);
+            Logger.LogWarning(message);
         }
 
         internal static void LogError(string message)
         {
-            I.LogErrorInternal(message);
+            Logger.LogError(message);
         }
 
         internal static void SetDirty(Object obj)
         {
-            I.SetDirtyInternal(obj);
+            MornGlobalUtil.SetDirty(obj);
         }
 
         internal static void LogAndSetDirty(string message, Object obj)
